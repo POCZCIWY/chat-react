5f5a51d2ae1c72872de17434693de52800104868
Hubert <hubertgrocholski8@gmail.com>
2026-04-18 12:20:15 +0200

    Babiczek

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// 1. Importujemy naszą nową wtyczkę
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  // Pamiętaj o SWOJEJ nazwie repozytorium na GitHubie!
-  base: '/chat-react/', 
+  base: '/chat-react/',
   plugins: [
     react(),
-    // 2. Dodajemy i konfigurujemy wtyczkę PWA
+
     VitePWA({
-      registerType: 'autoUpdate', // Aplikacja sama się zaktualizuje u użytkownika
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt'],
       manifest: {
-        name: 'Szkolny Shoutbox PRO',
-        short_name: 'Shoutbox',
-        description: 'Klasowy komunikator czasu rzeczywistego',
-        theme_color: '#8e44ad',
-        background_color: '#f4f7f6',
-        display: 'standalone',
+        name: 'Chat React',
+        short_name: 'Chat',
+        theme_color: '#1e1f22',
         icons: [
           {
-            src: 'icon-192.png',
+            src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icon-512.png',
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
